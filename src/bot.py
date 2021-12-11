1c64311b500c1215c2e07d4e9730f7a92a907cc7
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2021-12-11 16:29:30 +0700

    Market

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,19 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext
 
 print("Bot is working!")
 
 users = {
     1233164455: {
         "balance": 100,
-        "tokens": []
+        "tokens": {}
     }
 }
 market = {
     '123': {
         "user_id": 1233164455,
         "price": 10,
-        "name": 'Hook'
-    },
-    '233': {
-        "user_id": 1233164455,
-        "price": 15,
-        "name": 'Rare'
-    },
-    '2753': {
-        "user_id": 1233164455,
-        "price": 30,
-        "name": 'Mythical'
-    },
-    '342348': {
-        "user_id": 1233164455,
-        "price": 40,
-        "name": 'Arcane'
+        "url": 'http://techslides.com/demos/sample-videos/small.mp4'
     }
 }
 
@@ -36,9 +22,8 @@ def start(update, context):
     if not users.get(update.effective_user.id):
         users[update.effective_user.id] = {
             "balance": 100,
-            "tokens": []
+            "tokens": {}
         }
-
     message = "Привет, я Оки Токен~ Напиши /help, чтобы узнать больше."
     update.message.reply_text(message)
 
@@ -57,17 +42,16 @@ def help_command(update, context):
 def tockens_command(update, context):
     print(users)
     print(users[update.effective_user.id])
-    if users[update.effective_user.id]['tokens'] != []:
+    if users[update.effective_user.id]['tokens'] != {}:
         message = 'Вот твои токены~\n\n'
+        update.message.reply_text(message)
         for token in users[update.effective_user.id]['tokens']:
-            message += str(token) + "\n"
-            # for key in i:
-            #     message += key
-            #     message += ' - ' + str(i[key]) + ' кредитов\n'
-            #     break
+            context.bot.send_video(update.message.chat.id, users[update.effective_user.id]['tokens'][token]['url'])
+            message = str(token) + "\n"
+            update.message.reply_text(message)
     else:
         message = 'Ой, у тебя еще нет токенов( Ты можешь купить их, написав /buy\n'
-    update.message.reply_text(message)
+        update.message.reply_text(message)
 
 
 def sell_command(update, context):
@@ -77,12 +61,12 @@ def sell_command(update, context):
 
 
 def buy_command(update, context):
-    message = "Вот игровые предметы~\n"
+    message = "Вот видео~\n\n\n"
     update.message.reply_text(message)
 
     for key in market:
-        message = 'Название : ' + market[key]['name'] + '\n\n'
-        message += 'Цена:' + str(market[key]['price']) + '\n\n'
+        context.bot.send_video(update.message.chat.id, market[key]['url'])
+        message = 'Цена: ' + str(market[key]['price']) + '\n\n\n'
         message += 'Чтобы купить напиши buy_' + str(key) + '\n\n\n'
         update.message.reply_text(message, parse_mode="HTML")
 
@@ -98,7 +82,10 @@ def unknown_command(update, context):
             message += 'Вот ваш токен: ' + token + '\n'
             users[update.effective_user.id]['balance'] -= price
             users[market[token]["user_id"]]['balance'] += price
-            users[update.effective_user.id]['tokens'].append(token)
+
+            users[update.effective_user.id]['tokens'][token] = {
+                'url': market[token]['url'],
+            }
 
             market.pop(token, None)
             message += 'Ваш баланс: ' + str(users[update.effective_user.id]['balance']) + '\n'
@@ -114,7 +101,8 @@ def unknown_command(update, context):
 
         market[token] = {
             "price": price,
-            "user_id": update.effective_user.id
+            "user_id": update.effective_user.id,
+            'url': users[update.effective_user.id]['tokens'][token]['url']
         }
 
         message = 'Успешно выставлен на продажу!\n\n'
